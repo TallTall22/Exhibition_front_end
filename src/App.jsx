d3f3232089c3ffd2086babe337442ffac9bb463d
TallTall22 <syuns1205@gmail.com>
2023-06-07 01:31:15 +0800

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css';
-import { Button } from 'react-bootstrap';
+import './App.scss';
+import Header from './components/header/header';
 import './reset.css'
 function App() {
   return (
     <div className="App">
-      <Button>hsdjfk</Button>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,28 @@
+import {Nav, Navbar} from 'react-bootstrap'
+import style from './header.module.scss'
+function  Header(){
+  return (
+    <header>
+      <Navbar className={style.navbar}>
+          <h1 className={style.navbarBrand}><a href='/'>文物展覽網</a></h1>
+          <input type='checkbox' className={style.navbarToggle} id='navbar-toggle'/>
+        <Nav className={style.nav} as='ul'>
+          <Nav.Item className={style.navItem} as='li'>     
+              <Nav.Link className={style.navLink}>展覽</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className={style.navItem} as='li'>    
+              <Nav.Link className={style.navLink}>影片專區</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className={style.navItem} as='li'>    
+              <Nav.Link className={style.navLink}>會員專區</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <label for='navbar-toggle' className={style.navbarToggleLabel}>
+          <span className={style.hamberger}></span>
+        </label>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
