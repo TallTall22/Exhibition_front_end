99e87aad56bc7747da178537572253b20e4a68c1
TallTall22 <syuns1205@gmail.com>
2023-06-06 18:27:02 +0800

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+import './reset.css'
+function App() {
+  return (
+    <div className="App">
+      <Button>hsdjfk</Button>
+    </div>
+  );
+}
+
+export default App;
